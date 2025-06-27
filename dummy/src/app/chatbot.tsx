86da32b495a4fd6d4ddefe84e84aff5694efd02f
vedant-inamdar <vsinamdar8787@gmail.com>
2025-06-27 11:53:03 +0530

    updates new

--- dummy/src/app/chatbot.tsx
+++ dummy/src/app/chatbot.tsx
@@ -8,6 +8,20 @@ const Chatbot: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+
+  const handleImageClick = () => {
+    if (showChatbot) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setShowChatbot(false);
+        setIsClosing(false);
+      }, 300);
+    } else {
+      setShowChatbot(true);
+    }
+  };
+
   const apiKey = process.env.NEXT_PUBLIC_APIKEY;
   const chatbotUrl = "https://ai.nexus.corecotechnologies.com/chatbot-tenant";
 
@@ -16,10 +30,6 @@ const Chatbot: React.FC = () => {
       setIsLoading(true);
     }
   }, [showChatbot]);
-
-  const handleImageClick = () => {
-    setShowChatbot(!showChatbot);
-  };
 
   const handleIframeLoad = () => {
     if (iframeRef.current) {
@@ -36,7 +46,11 @@ const Chatbot: React.FC = () => {
       <h1>Dummy Next.js TypeScript App</h1>
 
       {showChatbot && (
-        <div className={styles.chatbotWrapper}>
+        <div
+          className={`${styles.chatbotWrapper} ${
+            isClosing ? styles.chatbotClosing : ""
+          }`}
+        >
           <iframe
             ref={iframeRef}
             title="Chatbot"
@@ -49,13 +63,18 @@ const Chatbot: React.FC = () => {
         </div>
       )}
 
-      <div className={styles.chatbotToggleBtn} onClick={handleImageClick}>
+      <div
+        className={`${styles.chatbotToggleBtn} ${
+          showChatbot ? styles.hideLabel : ""
+        }`}
+        onClick={handleImageClick}
+      >
         <img
           className={showChatbot ? styles.imgClose : styles.imgChat}
           src={
             showChatbot
               ? "https://cdn-icons-png.flaticon.com/512/1828/1828778.png"
-              : "https://cdn-icons-png.flaticon.com/512/2462/2462719.png"
+              : "https://cdn-icons-png.flaticon.com/512/4712/4712027.png"
           }
           alt="Toggle Chatbot"
         />
